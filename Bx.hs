fb0497d5c02ba119baa5e5950b3a4bd400320d2d
Lisandra <lisandrasilva18@hotmail.com>
2019-04-19 18:10:07 +0900

    Put function

--- Bx.hs
+++ Bx.hs
@@ -11,6 +11,13 @@ import Examples
 import ShowAut
 import Ndfa2Dfa
 
+putNdfa :: NdfaT (Indexed Char) Char 
+        -> DfaT [Indexed Char] Char
+        -> Either String (NdfaT (Indexed Char) Char)
+putNdfa ndfa@(NdfaT v1 q1 s1 z1 delta1) dfa@(DfaT v2 q2 s2 z2 delta2) = 
+    if wellBuilt dfa then Right (putNdfa' ndfa 0 dfa)
+    else Left "View is not correct"
+
 {-  Checks if a DFA is well built (assuming that it was originated from a glushkov Ndfa 
     with powerset construction):
       - Every transition from (o,s) is unique
@@ -42,100 +49,41 @@ reachableNodes delta (n:ns) acc = reachableNodes delta x (n:acc)
     where x = ns `union` [ d | ((o,y),d) <- delta , o == n , not (d `elem` (n:acc))]
 
 
-{-  Checks if for each entry in the Dfa's table transition the walk in the Ndfa's transition table from
-    from the origins gives the same set, which means if a given transition in the Dfa was modified or not
-
-getModification :: (Ord st, Eq sy) => [((st,sy), [st])] -> [(([st],sy), [st])] -> Maybe (([st], sy), [st])
-getModification _ [] = Nothing
-getModification delta (((os,y),ds):xs) = if (from delta os y) == (nub $ sort ds)
-                                            then getModification delta xs
-                                         else Just ((os,y),ds)
-    where from d ls x = nub $ sort [s' | ((i,y),s) <- d, y == x, i `elem` ls, s' <- s]
--}
-
-{-  Checks if a given entry in the Dfa's table transition was modified or not -}
-viewModified :: (Ord st, Eq sy) => [((st,sy), [st])] -> (([st],sy), [st]) -> Bool
-viewModified delta ((os,y),ds) = (from delta os y) == (nub $ sort ds)
-    where from d ls x = nub $ sort [s' | ((i,y),s) <- d, y == x, i `elem` ls, s' <- s]
-
-
--- Preciso do s2 porque o e-closure do NDFA == s2 e preciso do z2 porque todos os estados em q2 têm que ter um caminho até z2
--- Pôr isto a retornar erro caso o automato não esteja bem contruído
-{-
- - tenho a delta_ndfa e a delta_dfa
- - pego na cabeça do ndfa e calculo o walk, procuro 
-
- Case:
-  normal 0 ambas as delta transitions tables estão vazias -- aplico o bigul aos estados, vocabulário e estados finais
-  normal 1 [ndfawalk destinos = view ] [ndfawalk dest length = 1] -- aplico um bigul
-  normal 2 [ndfawalk destinos = view ] [ndfawalk dest length > 1] -- primeiro tenho que trazer para a cabeça todas as transiçoes rearranjar a source e aplicar outro bigul
-  adaptive 1 source_empty view not -- create on the source the transitions
-  adaptive 2 source view empty -- return a empty source
-
-Ndfa
-Vocabulary:     'b', 'a'
-States:         b2, a1, a0
-Start state(s): _0
-Final states:   _0, a0, a1, b2
-Transitions:    (_0,'a') -> a1
-                (_0,'a') -> a2
-                (_0,'b') -> b2
-                (b2,'b') -> b2
-                (a1,'b') -> b2
-                (a0,'a') -> a1
-                (a0,'b') -> b2
-
-Transitions:    (a2,'b') -> [b3]
-                (b3,'b') -> [b3]
-                (_0,'b') -> [b3]
-                (_0,'a') -> [a2]
-
-
-
-                (a1,'a') -> [a2]
-                (a1,'b') -> [b3]
-
-4
-
-Dfa
-Vocabulary:     'b', 'a', 'c'
-States:         [a2], [a1,a2], [b3], [_0]
-Start state(s): [_0]
-Final states:   [a2], [a1,a2], [b3], [_0]
-
-Transitions:    ([a2],'b') -> [b3]:
-                ([b3],'b') -> [b3]:
-                ([_0],'b') -> [b3]:
-                ([_0],'a') -> [a2]
-
--}
-
-putNdfa' :: Eq st => NdfaT st sy -> DfaT [st] sy -> NdfaT st sy
-putNdfa' (NdfaT v1 q1 s1 z1 []) (DfaT v2 q2 s2 z2 []) = (NdfaT v q s z [])
+putNdfa' :: (Ord st, Eq st, Eq sy) => NdfaT st sy -> Int -> DfaT [st] sy -> NdfaT st sy
+putNdfa' (NdfaT v1 q1 s1 z1 d1) n (DfaT v2 q2 s2 z2 []) = (NdfaT v q s z d)
     where v = v2
           q = nub $ concat q2
           s = s1
           z = f z1 (concat z2)
+          d = take n d1
           f [] cz2 = cz2
-          f (h:t) cz2 = if h `elem`cz2 then h:(f t (filter (/= h) cz2))
+          f (h:t) cz2 = if h `elem` cz2 then h:(f t (filter (/= h) cz2))
                         else f t cz2
-putNdfa' (NdfaT v1 q1 s1 z1 d) dfa@(DfaT v2 q2 s2 z2 []) = putNdfa' (NdfaT v1 q1 s1 z1 []) dfa
-putNdfa' (NdfaT v1 q1 s1 z1 []) dfa@(DfaT v2 q2 s2 z2 (d:ds)) = putNdfa' (NdfaT v1 q1 s1 z1 daux) dfa
-    where daux = rearrangeS d
-          -- mudar aqui no primeiro caso também temos que ter cuidado que ele não vá por outra
-          -- rearrangeS (([o],sy),dsts) = [((o,sy),dsts)]
-          rearrangeS ((os,sy),dsts)  = case [((o,sy),dsts) | o <- os \\ (concat $ delete os q2)] of
-                                       [] -> error "View not consistent with source"
-                                       x  -> x
---putNdfa' (NdfaT v1 q1 s1 z1 d1) @dfa(DfaT v2 q2 s2 z2 d2) = putNdfa' (NdfaT v1 q1 s1 z1 d1') @dfa(DfaT v2 q2 s2 z2 [])
---    where d1' = reflectS d1 d2 0
+putNdfa' (NdfaT v1 q1 s1 z1 delta1) n (DfaT v2 q2 s2 z2 (d2:ds2)) = 
+      putNdfa' (NdfaT v1 q1 s1 z1 (rel2Func d1')) nn (DfaT v2 q2 s2 z2 ds2)
+    where delta = func2Rel delta1
+          (d1', nn) = if viewModified delta d2 then (sc ++ newEdges, n + length newEdges)
+                      else (sc ++ c ++ e, n + length c)
+          (sc,ukn) = splitAt n delta
+          (c,e) = splitEdges ukn d2
+          newEdges = (rearrangeS d2 q2) \\ sc
 
+{-  Checks if a given entry in the Dfa's table transition was modified or not -}
+viewModified :: (Ord st, Eq sy) => [((st,sy), st)] -> (([st],sy), [st]) -> Bool
+viewModified delta ((os,y),ds) = (from delta os y) == (nub $ sort ds)
 
-putNdfa :: NdfaT (Indexed Char) Char 
-        -> DfaT [Indexed Char] Char
-        -> Either String (NdfaT (Indexed Char) Char)
-putNdfa ndfa@(NdfaT v1 q1 s1 z1 delta1) dfa@(DfaT v2 q2 s2 z2 delta2) = 
-    if wellBuilt dfa then Right (putNdfa' ndfa dfa)
-    else Left "View is not correct"
+from :: (Ord st, Eq sy) => [((st,sy), st)] -> [st] -> sy -> [st]
+from delta states symbol = nub $ sort [s | ((i,y),s) <- delta, y == symbol, i `elem` states]
 
+splitEdges ::  (Eq st, Eq sy) => [((st,sy), st)] -> (([st],sy), [st]) -> ([((st,sy), st)],[((st,sy), st)])
+splitEdges [] _ = ([],[])
+splitEdges (edge@((o,y),d):t) ((os,symb),ds) = 
+    let (c,e) = splitEdges t ((os,symb),ds)
+    in if o `elem` os && y == symb && d `elem` ds then (edge:c,e)
+       else (c,edge:e)
+
+rearrangeS :: Eq st => (([st], sy), [st]) -> [[st]] -> [((st, sy), st)]
+rearrangeS ((os,sy),dsts) q = case [((o,sy),dd) | o <- os \\ (concat $ delete os q), dd <- dsts] of
+                             [] -> error "View not consistent with source"
+                             x  -> x
 
